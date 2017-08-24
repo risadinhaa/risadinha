0636fbe40fac41a6f9e515b6e6ecc804273daad0
samara <risadinhasistema@gmail.com>
2017-08-24 10:48:57 -0300

    cadastrar co sucesso

--- cadastrar.php
+++ cadastrar.php
@@ -5,7 +5,7 @@
 	$nome= $_POST ["txt_nome_cadastrar"];//atribuição do campo "nome" vindo do formulário para variavel
 	$email= $_POST ["txt_email_cadastrar"];//atribuição do campo "email" vindo do formulário para variavel
 	$senha= $_POST ["txt_senha_cadastrar"];//atribuição do campo "senha" vindo do formulário para variavel
-	$data_de_nascimento= $_POST ["txt_data_de_nascimento_cadastrar"];//atribuição do campo "nome" vindo do formulário para variavel
+	$data_de_nascimento= $_POST ["txt_data_nascimento_cadastrar"];//atribuição do campo "nome" vindo do formulário para variavel
 	$apelido= $_POST ["txt_apelido_cadastrar"];//atribuição do campo "email" vindo do formulário para variavel
 
 	//Gravando no banco de dados ! conectando com o localhost - mysql
@@ -19,9 +19,10 @@
         $row = $result->fetch_row();
         if ($row[0] == null || $row[0] == "") {
             
-    	    $query = "INSERT INTO users (nome, email, senha, apelido, data_de_nascimento) VALUES (\"$nome\", \"$email\", \"$senha\", \"$apelido\", \"$data_de_nascimento\")";
+    	    $query = "INSERT INTO users (nome, email, senha, apelido, data_de_nascimento) VALUES (\"$nome\", \"$email\", \"$senha\", \"$apelido\", STR_TO_DATE(\"$data_de_nascimento\", \"%d/%m/%Y\"))";
+    	    echo $query, "<br>";
     	    if($result = mysqli_query($conexao,$query))
-    	        echo "Seu cadastro foi realizado com sucesso!"; 
+    	        header("Location: estamos_trabalhando.html"); 
 					           
 			else
                 echo "erro."; 
---
+++ login.php
@@ -0,0 +1,33 @@
+<?php
+
+	
+// RECEBENDO OS DADOS PREENCHIDOS DO FORMULÁRIO !
+	$email= $_POST ["txt_email"];//atribuição do campo "email" vindo do formulário para variavel
+	$senha= $_POST ["txt_senha_logar"];//atribuição do campo "senha" vindo do formulário para variavel
+	
+	//Gravando no banco de dados ! conectando com o localhost - mysql
+	$conexao = mysqli_connect("localhost","root","root","risadinha"); //localhost é onde esta o banco de dados.
+	if (!$conexao)
+	    die ("Erro de conexão com localhost, o seguinte erro ocorreu -> ".mysqli_connect_error()); 
+ 
+	//conectando com a tabela do banco de dados
+    $query = "SELECT apelido from users where email=\"$email\" and senha=\"$senha\";
+    if ($result = mysqli_query($conexao,$query)) {
+        $row = $result->fetch_row();
+        if ($row[0] == null || $row[0] == "") {
+            
+    	    
+    	    echo "<br>login/senha invalidos";
+    	    
+					
+        } else {
+            echo "<br>vai login: \"", $row[0],"\"."; 
+        }
+    } else {
+        echo "bug ao consultar o BD.";  
+			
+    }
+    $result->close();
+    mysqli_close($conexao);
+	
+?>
